774843a8f92ae23313f004cb6c6e260342281bc5
Eponick <mikehuntwife@gmail.com>
2018-04-04 15:19:00 -0400

    Done, without string::tr

--- crypt.rb
+++ crypt.rb
@@ -12,6 +12,15 @@ class String
     end
 
     def Decrypt
+        return self.split("").collect{ |c|
+            if(("a".."z").cover?(c))
+                (((c.ord+6-'a'.ord)%26)+'a'.ord).chr
+            elsif(("A".."Z").cover?(c))
+                (((c.ord+6-'A'.ord)%26)+'A'.ord).chr
+            else
+                c
+            end
+        }.join("")
         lower = self.tr("a-z", "g-za-f")
         return lower.tr("A-Z", "G-ZA-F")
     end
